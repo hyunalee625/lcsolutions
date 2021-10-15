f64fcebf67c2cc855747a6777e07cd5449f8aa93
Lauren <hyunalee625@gmail.com>
2021-10-15 19:16:25 +0900

    review

--- addTwoNums.js
+++ addTwoNums.js
@@ -1,3 +1,26 @@
+// var addTwoNums = function(l1, l2) {
+//     let newNode = new ListNode(0);
+//     let head = newNode;
+//     let carry = 0;
+
+//     while (l1 || l2) {
+//         let a = l1 && l1.val;
+//         let b = l2 && l2.val;
+//         let sum = a + b + carry;
+
+//         carry = Math.floor(sum/10);
+//         head.next = new ListNode(sum % 10);
+
+//         if(l1) l1 = l1.next;
+//         if(l2) l2 = l2.next;
+
+//         head = head.next;
+//     }
+
+//     if (carry > 0) head.next = new ListNode(carry);
+//     return newNode.next;
+// }
+
 var addTwoNums = function(l1, l2) {
     let newNode = new ListNode(0);
     let head = newNode;
@@ -20,3 +43,4 @@ var addTwoNums = function(l1, l2) {
     if (carry > 0) head.next = new ListNode(carry);
     return newNode.next;
 }
+
--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,4 +1,20 @@
-function longestCommonPrefix(strs) {
+// function longestCommonPrefix(strs) {
+//     let result = strs[0];
+
+//     for (let i = 1; i < strs.length; i++) {
+//         const ele = strs[i];
+//         while (!ele.startsWith(result)) {
+//             result = result.substring(0, result.length - 1)
+//         }
+//         if (result = "") break;
+//     }
+
+//     return result;
+// }
+
+// console.log(longestCommonPrefix(["flower", "flow", "flight"]))
+
+function longestCommPrefix(strs) {
     let result = strs[0];
 
     for (let i = 1; i < strs.length; i++) {
@@ -11,5 +27,3 @@ function longestCommonPrefix(strs) {
 
     return result;
 }
-
-// console.log(longestCommonPrefix(["flower", "flow", "flight"]))
