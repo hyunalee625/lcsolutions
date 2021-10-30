474fbb69ca5c2215c40bfe93fbf1b3bd399f6d3e
Lauren <hyunalee625@gmail.com>
2021-10-30 22:00:02 +0900

    review

--- addBinary.js
+++ addBinary.js
@@ -1,3 +1,30 @@
+// function addBinary(a, b) {
+//     let aReverse = a.split("").reverse().map(Number);
+//     let bReverse = b.split("").reverse().map(Number);
+//     let rest = 0;
+//     let j = 0;
+//     let res = [];
+
+//     while (j < a.length || j < b.length) {
+//         let aNum = aRev[j] || 0;
+//         let bNum = bRev[j] || 0;
+//         sum = aNum + bNum + rest;
+
+//         if (sum === 3) {
+//             res.push(1);
+//             rest = 1
+//         } else if (sum === 2) {
+//         res.push(0);
+//         rest = 1;
+//         } else {
+//             res.push(sum);
+//             rest = 0
+//         }
+//     }
+
+//     j++;
+// }
+
 function addBinary(a, b) {
     let aReverse = a.split("").reverse().map(Number);
     let bReverse = b.split("").reverse().map(Number);
@@ -6,8 +33,8 @@ function addBinary(a, b) {
     let res = [];
 
     while (j < a.length || j < b.length) {
-        let aNum = aRev[j] || 0;
-        let bNum = bRev[j] || 0;
+        let aNum = aReverse[j] || 0;
+        let bNum = bReverse[j] || 0;
         sum = aNum + bNum + rest;
 
         if (sum === 3) {
@@ -24,3 +51,5 @@ function addBinary(a, b) {
 
     j++;
 }
+
+console.log(addBinary("11", "1"))
