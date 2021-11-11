bc53c62de8341c6f10adf4e7d7780208500377d5
Lauren <hyunalee625@gmail.com>
2021-11-11 23:31:26 +0900

    Question attempt

--- lcpn.js
+++ lcpn.js
@@ -1,5 +1,16 @@
-function letterComboPhoneNum(digits) {
+var letterCombo = function(digits) {
+    const numLetter = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    };
+
 
 }
 
-// 
+// digits range ['2', '9']
